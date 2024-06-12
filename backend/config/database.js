7f4ef6e5c7b49bd1d9cf5c17d4e561d96d01c781
Talha8840 <talha@pipesort.com>
2024-06-12 08:06:51 -0700

    fix error

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  database : 'eventdb_jd6v',
-  username: 'eventdb_jd6v_user',
-  password: 'Sw2AGlM5iVDPvIIvDvj9ikxUUHrHJYGp',
-  host: 'dpg-cpkppqi0si5c73cviv60-a.oregon-postgres.render.com', 
-  port: 5432, 
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true // This should be set to true in production with a valid SSL certificate
+    }
+  }
 });
 
 module.exports = sequelize;
